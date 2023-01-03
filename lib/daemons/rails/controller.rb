0a9410206dc0e79466d6398d076dada044c8dd8d
Ravi Mariya <rm9492@outlook.com>
2023-01-03 22:22:09 +0530

    initial commit

---
+++ lib/daemons/rails/controller.rb
@@ -0,0 +1,30 @@
+module Daemons
+  module Rails
+    class Controller
+      attr_reader :path, :app_name
+
+      def initialize(controller_path)
+        @path = controller_path
+        @app_name = "#{controller_path.basename.to_s[0...-'_ctl'.length]}.rb"
+      end
+
+      def run(command, argv={})
+        arguments = '-- '
+        argv.each {|key,value| arguments += "#{key} #{value} "}
+        `cd "#{Daemons::Rails.configuration.root}" && "#{path}" #{command} #{arguments unless argv.empty?}`
+      end
+
+      def start(argv={})
+        run('start',argv)
+      end
+
+      def stop
+        run('stop')
+      end
+
+      def status
+        run('status').to_s.split("\n").last =~ /: running \[pid \d+\]$/ ? :running : :not_exists
+      end
+    end
+  end
+end
---
+++ lib/generators/templates/script.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+# You might want to change this
+ENV["RAILS_ENV"] ||= "production"
+
+root = File.expand_path(File.dirname(__FILE__))
+root = File.dirname(root) until File.exist?(File.join(root, 'config'))
+Dir.chdir(root)
+
+require File.join(root, "config", "environment")
+
+$running = true
+Signal.trap("TERM") do 
+  $running = false
+end
+
+Rails.logger.auto_flushing = true if Rails.logger.respond_to?(:auto_flushing)
+
+while $running do
+  # Replace this with your code
+  Rails.logger.info "This daemon is still running at #{Time.now}.\n"
+  
+  sleep 10
+end
